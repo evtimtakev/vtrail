face550e699794a0644026515fea22334ff8340f
vv034846 <vladimir.velikov@broadcom.com>
2024-08-27 01:58:26 +0300

    Export to PDF capability

--- web/vtrail-ui/src/app/social-media-result-grid/social-media-result-grid.component.ts
+++ web/vtrail-ui/src/app/social-media-result-grid/social-media-result-grid.component.ts
@@ -1,4 +1,6 @@
 import {Component, Input} from '@angular/core';
+import jsPDF from 'jspdf'
+import autoTable from 'jspdf-autotable'
 
 @Component({
   selector: 'app-social-media-result-grid',
@@ -9,4 +11,18 @@ export class SocialMediaResultGridComponent {
   @Input() gridItems: any[] = [];
 
   @Input() isLoading: boolean = false;
+
+  public exportToPdf(): void {
+
+    const doc = new jsPDF()
+    doc.text("Social Media Community Report", 100, 10, { align: "center" }, 2);
+
+    autoTable(doc, {
+      columnStyles: { 4: { cellWidth: 40, textColor: [173, 216, 230] } },
+      head: [['Title', 'Description', 'Creation date', 'Social Media', 'Link']],
+      body: this.gridItems.map(item => [item.title, item.description, item.created, item.type, item.url])
+    })
+
+    doc.save('vTrail.pdf')
+  }
 }
